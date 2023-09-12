4caea7ef3a08a23b9ff52953783514d4e356366f
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-09-12 15:28:48 +0100

    Closer to real implementation

--- src/Web/WebMVC/Views/Concierge/ChatState.cs
+++ src/Web/WebMVC/Views/Concierge/ChatState.cs
@@ -1,5 +1,8 @@
-﻿using Azure.AI.OpenAI;
-using Microsoft.AspNetCore.Components;
+﻿#define FAKE_OPENAI
+
+using System.Net;
+using System.Text.Json.Nodes;
+using Azure.AI.OpenAI;
 
 namespace WebMVC.Views.Concierge;
 
@@ -64,15 +67,86 @@ public class ChatState
 
         _completionsOptions.Messages.Add(new ChatMessage(ChatRole.Assistant, "Hi! I'm the .NET Concierge. How can I help?"));
 
-        //_client = new OpenAIClient(new Uri(aoaiEndpoint), new AzureKeyCredential(aoaiApiKey));
+#if !FAKE_OPENAI
+        _client = new OpenAIClient(new Uri(aoaiEndpoint), new AzureKeyCredential(aoaiApiKey));
+#endif
     }
 
     public IList<ChatMessage> Messages => _completionsOptions.Messages;
 
-    public async Task AddUserMessageAndGetResponseAsync(string text)
+    public async Task AddUserMessageAsync(string text, Action onMessageAdded)
     {
         _completionsOptions.Messages.Add(new ChatMessage(ChatRole.User, text));
-        await Task.Delay(1000);
-        _completionsOptions.Messages.Add(new ChatMessage(ChatRole.Assistant, "Yeah whatever"));
+        onMessageAdded();
+
+        try
+        {
+#if FAKE_OPENAI
+            FakeChatChoice choice;
+#else
+            ChatChoice choice;
+#endif
+            while (true)
+            {
+#if FAKE_OPENAI
+                await Task.Delay(4000);
+                choice = new FakeChatChoice
+                {
+                    FinishReason = CompletionsFinishReason.Stopped,
+                    Message = new ChatMessage(ChatRole.Assistant, "That's nice, thanks.")
+                };
+#else
+                var response = await _client.GetChatCompletionsAsync(aoaiModel, _completionsOptions);
+                choice = response.Value.Choices[0];
+#endif
+
+                _completionsOptions.Messages.Add(choice.Message);
+                onMessageAdded();
+
+                if (choice.FinishReason != CompletionsFinishReason.FunctionCall)
+                {
+                    return;
+                }
+
+                switch (choice.Message.FunctionCall.Name)
+                {
+                    case SearchCatalogFunctionName:
+                        string productDescription = JsonNode.Parse(choice.Message.FunctionCall.Arguments)?["product_description"]?.ToString();
+                        try
+                        {
+                            productDescription = await new HttpClient().GetStringAsync($"http://localhost:5222/api/v1/Catalog/items/withsemantic/{WebUtility.UrlEncode(productDescription)}?pageSize=3&pageIndex=0");
+                        }
+                        catch (HttpRequestException)
+                        {
+                            productDescription = "Error accessing catalog.";
+                        }
+                        _completionsOptions.Messages.Add(new ChatMessage(ChatRole.Function, productDescription) { Name = SearchCatalogFunctionName });
+                        onMessageAdded();
+                        continue;
+
+                    case AddToBasketFunctionName:
+                        // TODO: Call basket endpoint
+                        _completionsOptions.Messages.Add(new ChatMessage(ChatRole.Function, "Item added to shopping cart.") { Name = AddToBasketFunctionName });
+                        onMessageAdded();
+                        continue;
+
+                        // TODO: Could add other functions, e.g.
+                        // - Enumerate basket
+                        // - Remove from basket
+                }
+            }
+        }
+        catch (Exception)
+        {
+            _completionsOptions.Messages.Add(new ChatMessage(ChatRole.Assistant, "My apologies, but I encountered an unexpected error."));
+        }
     }
+
+#if FAKE_OPENAI
+    class FakeChatChoice
+    {
+        public CompletionsFinishReason FinishReason { get; set; }
+        public ChatMessage Message { get; set; }
+    }
+#endif
 }
