76954139cc7ef2c725c97f9c79ea446795589cc3
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-09-12 14:24:54 +0100

    Add messages dynamically

---
+++ src/Web/WebMVC/Views/Concierge/ChatMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace WebMVC.Views.Concierge;
+
+public class ChatMessage
+{
+    public ChatMessageType MessageType { get; set; }
+
+    public string Text { get; set; }
+
+    public enum ChatMessageType { Sent, Received };
+}
